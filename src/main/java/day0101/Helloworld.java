174a4b6ef09e106a2048a8e2eeaa74e4c8cf6d96
gudian1618 <1761844211@qq.com>
2020-11-25 20:46:13 +0800

    Initial commit

---
+++ src/main/java/day0101/Helloworld.java
@@ -0,0 +1,15 @@
+package day0101;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2020/11/25 8:45 下午
+ */
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
