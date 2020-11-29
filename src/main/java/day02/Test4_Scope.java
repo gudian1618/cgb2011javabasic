359a8b68dd94bffe87832d483efd01c7b1a52592
gudian1618 <1761844211@qq.com>
2020-11-29 18:08:38 +0800

    new

--- src/main/java/day02/Test4_Scope.java
+++ src/main/java/day02/Test4_Scope.java
@@ -12,6 +12,10 @@ public class Test4_Scope {
 
     public static void main(String[] args) {
 
+        long aa = 99999999999L;
+        float bb = 4.3F;
+        double cc = 30.989d;
+        
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
