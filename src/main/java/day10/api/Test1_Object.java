3ef9d4e0e0f57294b3892bee51ffe9bdd2b3d035
gudian1618 <1761844211@qq.com>
2020-12-21 22:58:49 +0800

    new

--- src/main/java/day10/api/Test1_Object.java
+++ src/main/java/day10/api/Test1_Object.java
@@ -15,8 +15,14 @@ public class Test1_Object {
 
         // toString() 返回该对象的字符串表示
         // 查看对象是否赋值成功
-        System.out.println(s);
         // 打印出来的是对象在内存中的地址值
+        // 原因:再打印对象时,底层会自动调用toString(),而且使用的是Object的提供的方法
+        System.out.println(s);
+        // 重写toString方法,是打印的对象的属性值,用idea自带的默认生成的重写toString方法,json格式
+        // 不重写打印的是对象的地址值,也就是hash值
+
+        // 返回对象在内存中的哈希码值
+        System.out.println(s.hashCode());
 
     }
 
@@ -32,6 +38,14 @@ class Student {
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "name='" + name + '\'' +
+            ", age=" + age +
+            '}';
     }
 }
 
