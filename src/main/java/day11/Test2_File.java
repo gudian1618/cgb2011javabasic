4dc9f2e186c2d5ccb0108c6e5021a73015b08a17
gudian1618 <1761844211@qq.com>
2020-12-27 19:45:05 +0800

    new

--- src/main/java/day11/Test2_File.java
+++ src/main/java/day11/Test2_File.java
@@ -2,11 +2,14 @@ package day11;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author gudian1618
  * @version v1.0
  * @date 2020/12/27 6:52 下午
+ * file.list列出文件夹中的所有文件和文件夹的名称
+ * file.listFiles列出文件夹中的所有文件的文件对象,可以调用该文件的所有功能和方法
  */
 
 public class Test2_File {
@@ -30,8 +33,16 @@ public class Test2_File {
         System.out.println(file1.mkdir());
         file1 = new File("src/main/java/day11/o/p");
         System.out.println(file1.mkdirs());
-        // 删除文件或空的文件夹
+        // 删除文件,或空的文件夹,有文件或文件夹的文件夹无法删除
         System.out.println(file1.delete());
+
+        file = new File("src/main/java/day11/");
+        // 列出文件夹中的所有文件的名称
+        String[] name = file.list();
+        System.out.println(Arrays.toString(name));
+        // 列出文件夹中的所有文件的文件对象
+        File[] fs = file.listFiles();
+        System.out.println(Arrays.toString(fs));
 
     }
 
