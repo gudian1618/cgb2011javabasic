7ec1d2ef346f6b648c1097e66a4f5f08c16c12cc
gudian1618 <1761844211@qq.com>
2020-12-28 21:56:47 +0800

    new

--- src/main/java/day11/file/Test3_DiGui.java
+++ src/main/java/day11/file/Test3_DiGui.java
@@ -11,6 +11,12 @@ import java.io.File;
  * 2.判断,当前资源是文件还是文件夹
  * -- 是文件,求文件字节量大小length()求和
  * -- 是文件夹,重复1,2步骤递归
+ *
+ * 递归删除文件夹
+ * 1.列出文件夹下的所有资源listFiles()
+ * 2.判断,当前资源是文件还是文件夹
+ * -- 是文件,直接删除文件delete()
+ * -- 是文件夹,重复1,2步骤递归
  */
 
 public class Test3_DiGui {
@@ -21,6 +27,28 @@ public class Test3_DiGui {
         // 2.求目录大小
         long total = size(file);
         System.out.println(total);
+
+        // 1.指定要求删除的文件夹
+        File file1 = new File("src/main/java/day11/m2");
+        // 2.创建删除文件夹的方法
+        del(file1);
+
+    }
+
+    private static void del(File file1) {
+        File[] fs = file1.listFiles();
+        for (int i = 0; i < fs.length; i++) {
+            File f = fs[i];
+            if (f.isFile()) {
+                // 直接调用删除文件的方法
+                f.delete();
+            } else if (f.isDirectory()) {
+                // 在方法内部调用自己
+                del(f);
+            }
+        }
+        // 在for循环之外,把所有的空文件夹直接删除
+        file1.delete();
     }
 
     private static long size(File file) {
