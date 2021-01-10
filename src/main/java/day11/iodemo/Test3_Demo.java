0b0399e555e2300e99b0f9184de03c722d840365
gudian1618 <1761844211@qq.com>
2021-01-10 18:03:05 +0800

    new

--- src/main/java/day11/iodemo/Test3_Demo.java
+++ src/main/java/day11/iodemo/Test3_Demo.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
  * @version v1.0
  * @date 2021/1/10 5:38 下午
  * 文件复制
+ * 字节流和字符流
+ * 字节流更为强大,字符流只针对文本数据
  */
 
 public class Test3_Demo {
@@ -23,6 +25,38 @@ public class Test3_Demo {
         File to = new File(t);
 
         copy(from, to);
+        copyZJ(from, to);
+    }
+
+    private static void copyZJ(File from, File to) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            // 1.读取from文件 -- 字节流
+            in = new BufferedInputStream(new FileInputStream(from));
+            // 2.写出到to文件
+            out = new BufferedOutputStream(new FileOutputStream(to));
+
+            // 边读边写
+            // 定义变量,存读到的数据
+            int b = 0;
+            // 没有数据就返回-1
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+            System.out.println("文件复制完成!");
+        } catch (IOException e) {
+            System.out.println("文件复制失败!");
+            e.printStackTrace();
+        } finally {
+            // 3.释放资源
+            try {
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static void copy(File from, File to) {
