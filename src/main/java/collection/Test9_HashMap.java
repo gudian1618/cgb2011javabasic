95300b4e054a1f277c2913be8ae35f520eefdee9
gudian1618 <1761844211@qq.com>
2021-01-12 22:48:04 +0800

    new

---
+++ src/main/java/collection/Test9_HashMap.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.HashMap;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/1/12 10:25 下午
+ */
+
+public class Test9_HashMap {
+
+    public static void main(String[] args) {
+        // 1.HashMap对象创建
+        HashMap<Integer, String> hashMap = new HashMap<>();
+
+
+
+    }
+
+}
