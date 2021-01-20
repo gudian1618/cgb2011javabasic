287f7a4185b4ed7c31af950ec8f32e1d10ac95f1
gudian1618 <1761844211@qq.com>
2021-01-20 20:56:15 +0800

    new

--- src/main/java/com/github/gudian1618/cgb2011javabasic/thread/Test7_Single2.java
+++ src/main/java/com/github/gudian1618/cgb2011javabasic/thread/Test7_Single2.java
@@ -4,13 +4,18 @@ package com.github.gudian1618.cgb2011javabasic.thread;
  * @author gudian1618
  * @version v1.0
  * @date 2021/1/20 8:29 下午
+ * 学习重点
  * 懒汉式单例模式: 本质就是延迟加载
  * 只提前声明,使用时创建,节省内存资源
+ * 1.延迟加载的思想: 什么时候用,什么时候创建
+ * 2.线程安全问题: 共享资源(成员变量)有并发的多线程数据隐患,加同步锁(锁方法或锁代码块)
  */
 
 public class Test7_Single2 {
     public static void main(String[] args) {
-
+        MySingleton my = MySingleton.getMy();
+        MySingleton my2 = MySingleton.getMy();
+        System.out.println(my == my2);
     }
 }
 
@@ -29,7 +34,7 @@ class MySingleton {
     // 如果方法里面的所有代码被同步,可以同步方法
     synchronized public static MySingleton getMy() {
         // 静态区域内不能使用this关键词,因为加载的先后顺序,静态资源优先于对象加载,加载不存在的对象,所以同步代码块不正确
-        if (my==null) {
+        if (my == null) {
             my = new MySingleton();
         }
         return my;
