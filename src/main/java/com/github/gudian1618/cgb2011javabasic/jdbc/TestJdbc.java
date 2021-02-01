13bf0fca3472d28baafd39f8f26403e8ac76bea4
gudian1618 <1761844211@qq.com>
2021-02-01 21:58:35 +0800

    new

--- src/main/java/com/github/gudian1618/cgb2011javabasic/jdbc/TestJdbc.java
+++ src/main/java/com/github/gudian1618/cgb2011javabasic/jdbc/TestJdbc.java
@@ -1,9 +1,6 @@
 package com.github.gudian1618.cgb2011javabasic.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * @author gudian1618
@@ -20,31 +17,65 @@ import java.sql.Statement;
 
 public class TestJdbc {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement stat = null;
+        ResultSet rs = null;
 
         // 1.注册数据库驱动
-        Class.forName("com.mysql.jdbc.Driver");
-        // 2.获取数据库连接
-        Connection conn = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/jt_db_test?characterEncoding=utf-8",
-            "root", "123456"
-        );
-        // 3.获取传输器
-        Statement stat = conn.createStatement();
-        // 4.发送sql到服务器执行并返回执行结果
-        String sql = "select * from account";
-        ResultSet rs = stat.executeQuery(sql);
-        // 5.处理结果
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            double money = rs.getDouble("money");
-            System.out.println(id + ":" + name + ":" + "money");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            // 2.获取数据库连接
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/jt_db_test?characterEncoding=utf-8",
+                "root", "123456"
+            );
+            // 3.获取传输器
+            stat = conn.createStatement();
+            // 4.发送sql到服务器执行并返回执行结果
+            String sql = "select * from account";
+            rs = stat.executeQuery(sql);
+            // 5.处理结果
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                double money = rs.getDouble("money");
+                System.out.println(id + ":" + name + ":" + "money");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 6.释放资源
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } finally {
+                    rs = null;
+                }
+            }
+            if (stat != null) {
+                // 6.释放资源
+                try {
+                    stat.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } finally {
+                    stat = null;
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } finally {
+                    conn = null;
+                }
+            }
         }
-        // 6.释放资源
-        rs.close();
-        stat.close();
-        conn.close();
 
         System.out.println("执行完成!");
     }
