da97609f4bdf8c0f1ab3ecd819dedd23ea46e559
gudian1618 <1761844211@qq.com>
2021-02-01 20:52:17 +0800

    new

---
+++ src/main/java/com/github/gudian1618/cgb2011javabasic/jdbc/TestJdbc.java
@@ -0,0 +1,52 @@
+package com.github.gudian1618.cgb2011javabasic.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/2/1 7:59 下午
+ * JDBC快速入门测试
+ * 1.注册数据库驱动
+ * 2.获取数据库连接
+ * 3.获取传输器
+ * 4.发送sql到服务器执行并返回执行结果
+ * 5.处理结果
+ * 6.释放资源
+ */
+
+public class TestJdbc {
+
+    public static void main(String[] args) throws Exception {
+
+        // 1.注册数据库驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2.获取数据库连接
+        Connection conn = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/jt_db_test?characterEncoding=utf-8",
+            "root", "123456"
+        );
+        // 3.获取传输器
+        Statement stat = conn.createStatement();
+        // 4.发送sql到服务器执行并返回执行结果
+        String sql = "select * from account";
+        ResultSet rs = stat.executeQuery(sql);
+        // 5.处理结果
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            double money = rs.getDouble("money");
+            System.out.println(id + ":" + name + ":" + "money");
+        }
+        // 6.释放资源
+        rs.close();
+        stat.close();
+        conn.close();
+
+        System.out.println("执行完成!");
+    }
+
+}
